b177b5984d79e80238ddc167a12e0c5e787e6cb1
Simone Biffi <simone.biffi.91@gmail.com>
2020-04-01 22:37:29 +0200

    feat: add store

--- src/index.js
+++ src/index.js
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
+import { createStore} from "redux";
+import calendar from "./reducers";
+
+const store = createStore(
+    calendar,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <App store={store}/>,
   document.getElementById('root')
 );
 
