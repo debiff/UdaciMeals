6a1b7912dcfb27538ad20a250a504567d728b742
Simone Biffi <simone.biffi.91@gmail.com>
2020-04-05 12:20:17 +0200

    feat: add react-redux provider

--- src/index.js
+++ src/index.js
@@ -4,15 +4,18 @@ import './index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 import { createStore} from "redux";
-import calendar from "./reducers";
+import reducer from "./reducers";
+import { Provider } from 'react-redux';
 
 const store = createStore(
-    calendar,
+    reducer,
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
     )
 
 ReactDOM.render(
-    <App store={store}/>,
+    <Provider store={store}>
+        <App />
+    </Provider>,
   document.getElementById('root')
 );
 
