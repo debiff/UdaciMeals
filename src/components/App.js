24042f6ba7047dc6bb96d0a5393ef9c52a79ee72
Simone Biffi <simone.biffi.91@gmail.com>
2020-04-05 12:21:34 +0200

    add: finalize the app

--- src/components/App.js
+++ src/components/App.js
@@ -1,47 +1,159 @@
 import React, { Component } from 'react'
-import calendar from "../reducers";
-import {addRecipe} from "../actions";
+import { connect } from 'react-redux'
+import {addRecipe, removeFromCalendar} from "../actions";
+import { capitalize } from "../utils/helpers";
+import { FaCalendarPlus, FaArrowCircleRight } from 'react-icons/fa'
+import Modal from 'react-modal'
+import Loading from "react-loading";
+import { fetchRecipes } from '../utils/api'
+import FoodList from "./FoodList";
 
 class App extends Component {
     state = {
-        calendar: null
+        foodModalOpen: false,
+        meal: null,
+        day: null,
+        food: null,
+        loadingFood: false
     }
-    componentDidMount() {
-        const { store } = this.props;
-
-        store.subscribe(() => {
-            this.setState({
-                calendar: store.getState()
-            })
-        })
-    }
-    submitFood = () => {
-        this.props.store.dispatch(addRecipe({
-            day: 'monday',
-            recipe: {
-                label: this.input.value
-            },
-            meal: 'breakfast'
+    openFoodModal = ({ meal, day }) => {
+        this.setState(() => ({
+            foodModalOpen: true,
+            meal,
+            day,
         }))
-        this.input.value = ''
     }
+    closeFoodModal = () => {
+        this.setState(() => ({
+            foodModalOpen: false,
+            meal: null,
+            day: null,
+            food: null,
+        }))
+    }
+    searchFood = (e) => {
+        if (!this.input.value) {
+            return
+        }
 
+        e.preventDefault()
+
+        this.setState(() => ({ loadingFood: true }))
+
+        fetchRecipes(this.input.value)
+            .then((food) => this.setState(() => ({
+                food,
+                loadingFood: false,
+            })))
+    }
     render() {
-        return (
-            <div>
-                <input
-                    type='text'
-                    ref={(input) => this.input = input}
-                    placeholder="Monday's Breakfast"
-                />
-                <button onClick={this.submitFood}>Submit</button>
+        const { foodModalOpen, loadingFood, food} = this.state
+        const { remove, calendar, selectRecipe } = this.props;
+        const mealOrder = ['breakfast', 'lunch', 'dinner'];
 
-                <pre>
-                  Monday's Breakfast: {this.state.calendar && this.state.calendar.monday.breakfast}
-                </pre>
+        return (
+            <div className='container'>
+                <ul className='meal-types'>
+                    {mealOrder.map((mealType) => {
+                        return(
+                            <li key={mealType} className='subheader'>
+                                {capitalize(mealType)}
+                            </li>)
+                    })}
+                </ul>
+                <div className='calendar'>
+                    <div className='days'>
+                        {calendar.map(({ day }) => <h3 key={day} className='subheader'>{capitalize(day)}</h3>)}
+                    </div>
+                    <div className='icon-grid'>
+                        {calendar.map(({ day, meals }) => (
+                            <ul key={day}>
+                                {mealOrder.map((meal) => (
+                                    <li key={meal} className='meal'>
+                                        {meals[meal]
+                                            ? <div className='food-item'>
+                                                <img src={meals[meal].image} alt={meals[meal].label}/>
+                                                <button onClick={() => remove({meal, day})}>Clear</button>
+                                            </div>
+                                            : <button className='icon-btn' onClick={() => this.openFoodModal({meal, day})}>
+                                                <FaCalendarPlus size={30}/>
+                                            </button>}
+                                    </li>
+                                ))}
+                            </ul>
+                        ))}
+                    </div>
+                </div>
+
+                <Modal
+                    className='modal'
+                    overlayClassName='overlay'
+                    isOpen={foodModalOpen}
+                    onRequestClose={this.closeFoodModal}
+                    contentLabel='Modal'
+                    ariaHideApp={false}
+                >
+                    <div>
+                        {loadingFood === true
+                            ? <Loading delay={200} type='spin' color='#222' className='loading' />
+                            : <div className='search-container'>
+                                <h3 className='subheader'>
+                                    Find a meal for {capitalize(this.state.day)} {this.state.meal}.
+                                </h3>
+                                <div className='search'>
+                                    <input
+                                        className='food-input'
+                                        type='text'
+                                        placeholder='Search Foods'
+                                        ref={(input) => this.input = input}
+                                    />
+                                    <button
+                                        className='icon-btn'
+                                        onClick={this.searchFood}>
+                                        <FaArrowCircleRight size={30}/>
+                                    </button>
+                                </div>
+                                {food !== null && (
+                                    <FoodList
+                                        food={food}
+                                        onSelect={(recipe) => {
+                                            selectRecipe({ recipe, day: this.state.day, meal: this.state.meal })
+                                            this.closeFoodModal()
+                                        }}
+                                    />)}
+                            </div>}
+                    </div>
+                </Modal>
+
             </div>
+
+
+
         )
     }
 }
 
-export default App
+function mapStateToProps({calendar, food}) {
+    const dayOrder = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday']
+    return {
+        calendar: dayOrder.map((day) => ({
+            day,
+            meals: Object.keys(calendar[day]).reduce((meals, meal) => {
+                meals[meal] = calendar[day][meal]
+                    ? food[calendar[day][meal]]
+                    : null
+                return meals
+            }, {})
+
+        }))
+    }
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        selectRecipe: (data) => dispatch(addRecipe(data)),
+        remove: (data) => dispatch(removeFromCalendar(data))
+    }
+
+}
+export default connect(mapStateToProps, mapDispatchToProps)(App)
